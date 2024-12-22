f6f9757d8445e72b48bb2966d9b4c8c0a0bf9f53
Zainab Aamir <zaamir427@gmail.com>
2024-12-22 23:29:08 +0500

    second commit

--- main.py
+++ main.py
@@ -16,3 +16,4 @@ Like a diamond in the sky
 Twinkle Twinkle Little Star
 How I wonder what you are''')
 
+print("this is the 5th programming language that i am learning and i pray that i do not leave it in the middle")
