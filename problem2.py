cccedc517476633d7d3dcd839a7256d64cb7dbd7
Zainab Aamir <zaamir427@gmail.com>
2024-12-23 17:14:37 +0500

    python exercise

---
+++ problem2.py
@@ -0,0 +1,30 @@
+#adding two numbers
+
+a = 1
+b = 3
+print(a+b)
+z = 4
+
+#finding remainder when a number is divided by z
+print(a%z)
+
+#Check the type of variable assigned using input () function.
+name = input("enter your name: ")
+print("your name is : ", name)
+print(type(name))
+
+#Use comparison operator to find out whether ‘c’ given variable a is greater than ‘d’ or not. Take c = 34 and d = 80
+
+c = 34
+d = 80 
+print(c > d)
+
+# Write a python program to find an average of two numbers entered by the user
+num1 = int(input("enter any number: "))
+num2 = int(input("enter another number: "))
+average = (num1 + num2)/2 
+print(average)
+
+#Write a python program to calculate the square of a number entered by the user
+myNum = int(input("enter any number: "))
+print(myNum ** 2)
