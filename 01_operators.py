a41338bb597605dad8ec20f2c205f9aabcb455b0
Zainab Aamir <zaamir427@gmail.com>
2024-12-23 16:45:08 +0500

    python operators

---
+++ 01_operators.py
@@ -0,0 +1,35 @@
+#There are several types of operators in python
+
+#arithmetic operator
+
+num3 = 12.3
+num4 = 32.4
+print( num3 + num4) 
+print(num3 * num4)
+print(num3 / num4)
+print(num3 + num4)
+
+#assignment operator
+num1 = 3
+num1 += 2
+print(num1)
+
+#comparison operator, they will always give boolean output
+print(num3 == num4)
+print(num4 > num3)
+print(num3 < num4)
+print(num3 <= num4)
+print(num4 >= num4)
+print(num3 != num4)
+
+#logical operators, they will always give boolean output
+print(num3 == num4 and num4 != num3)
+print(num3 == num4 or num4 < num3)
+print(not(num4 != num3 or num4 > num3))
+
+
+
+
+
+
+
