16e9921c3fac054fcfdea40e1b067a94e09498fa
Zainab Aamir <zaamir427@gmail.com>
2024-12-23 17:13:57 +0500

    typecasting

---
+++ typecasting.py
@@ -0,0 +1,29 @@
+#typecasting
+
+name = "zainab"
+age = 21
+isAdult = True
+
+print(type(name))
+print(type(age))
+print(type(isAdult))
+
+#type conversion
+
+a = int("2")
+print(type(a))
+b = str(12)
+print(type(b))
+c = float(32)
+print(type(c))
+
+#user input
+your_name = input("please enter your name")
+print("your name is", your_name)
+
+#user input will always return string even if you enter any number
+number = input("enter first number: ")
+number1 = input("enter the second number: ")
+print(number+ number1)
+
+
