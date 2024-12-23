e2128b22132255092c4ec40c6c2c64a4c8d1d30d
Zainab Aamir <zaamir427@gmail.com>
2024-12-24 00:14:15 +0500

    python exercise

---
+++ problem3.py
@@ -0,0 +1,19 @@
+# Write a python program to display a user entered name followed by Good
+# Afternoon using input () function.
+name = input("enter your name: ")
+print("good afternoon", name)
+
+# Write a program to detect double space in a string.
+names = "zainab  amir  khan"
+spaceCheck = names.count(" ")
+print(spaceCheck)
+
+#Replace the double space from problem 3 with single spaces
+replaceCommas = names.replace("" , '')
+print(names)
+
+#. Write a program to format the following letter using escape sequence characters.
+# letter = "Dear Zainab, this python course is nice. Thanks!"
+print("Dear Zainab,\nThis is python course is nice.\nThanks!")
+
+
