c28cfed0e49dcf2294968ffbfad74c9037aab462
Zainab Aamir <zaamir427@gmail.com>
2024-12-24 23:14:26 +0500

    sets in python

---
+++ sets.py
@@ -0,0 +1,25 @@
+#sets in python
+mySets = {21, "zainab", "safa", True, False, 12.3}
+print(mySets)
+print(type(mySets))
+print(len(mySets))
+
+#set constructor
+newSet = set(("sana", "safa", "zainab"))
+print(newSet)
+print(type(newSet))
+
+#set methods
+print(len(newSet))
+print(newSet.remove("sana"))
+print(newSet.clear())
+
+
+#pop method
+a = {"Sana", "safa", "zainab"}
+a.pop()
+print(a)
+
+
+
+
