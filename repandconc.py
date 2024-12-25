b14dd48daadd4b88158d05ccfbde1f0008aa220c
Zainab Aamir <zaamir427@gmail.com>
2024-12-25 09:43:22 +0500

    string replication

--- repandconc.py
+++ repandconc.py
@@ -1,6 +1,6 @@
 #string concatenation and replication
-print("python" *5) #string replication
-print(5* "zainab") #same as above
+print("python " *5) #string replication
+print(5* " zainab") #same as above
 
 print("zainab","sana") #string concatenation
 
