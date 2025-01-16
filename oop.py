f2b237a2c2702641a2466d627a4607bff7306a2c
Zainab Aamir <zaamir427@gmail.com>
2025-01-16 16:27:36 +0500

    oops in python

---
+++ oop.py
@@ -0,0 +1,35 @@
+#creting class
+class Employee:
+    name = "safaa"
+
+#creating object
+E_1 = Employee()
+print(E_1.name)
+
+
+#creating class
+class dresses:
+    color = "red"
+    brand = "Z-Wears"
+
+#creating object
+dress1 = dresses()
+print(dress1.brand)
+print(dress1.color)
+
+
+
+#creating object constructor
+class Student:
+    def __init__(self):
+        sum=2+2
+        print(sum)
+
+s1 = Student()
+
+
+
+    
+
+
+
