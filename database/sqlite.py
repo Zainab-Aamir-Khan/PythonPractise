58646f7e358634963e3c74393532abc002577170
Zainab Aamir <zaamir427@gmail.com>
2025-02-01 22:22:26 +0500

    sqlite3 practice

--- database/sqlite.py
+++ database/sqlite.py
@@ -4,11 +4,13 @@ conn = sqlite3.connect('employee.db')
 
 c = conn.cursor()
 
-c.execute("""CREATE TABLE employees(
-        first name 
-        last name
-        pay
-        )""")
+# c.execute("""CREATE TABLE employees(
+#         first name 
+#         last name
+#         pay
+#         )""")
+
+c.execute("SELECT * FROM employees WHERE last= 'Neena'")
 
 conn.commit()
 
