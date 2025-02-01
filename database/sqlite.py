221b1d2e8ba133e08364be93df7b0ed1f269aa2f
Zainab Aamir <zaamir427@gmail.com>
2025-02-01 22:32:16 +0500

    SQLite3

--- database/sqlite.py
+++ database/sqlite.py
@@ -5,8 +5,8 @@ conn = sqlite3.connect('employee.db')
 c = conn.cursor()
 
 # c.execute("""CREATE TABLE employees(
-#         first name 
-#         last name
+#         first name, 
+#         last name,
 #         pay
 #         )""")
 
