6098ecb42c38f9b82ccffab38fa4fb0860a3321c
Zainab Aamir <zaamir427@gmail.com>
2025-02-01 22:57:38 +0500

    sqlite3

--- database/sqlite.py
+++ database/sqlite.py
@@ -20,31 +20,3 @@
 # conn.commit()
 
 # conn.close()
-
-
-
-
-
-import sqlite3
- 
-# Connecting to sqlite
-# connection object
-connection_obj = sqlite3.connect('geek.db')
- 
-# cursor object
-cursor_obj = connection_obj.cursor()
- 
-# Drop the GEEK table if already exists.
-cursor_obj.execute("DROP TABLE IF EXISTS GEEK")
- 
-# Creating table
-table = """ CREATE TABLE GEEK (
-            Email VARCHAR(255) NOT NULL,
-            First_Name CHAR(25) NOT NULL,
-            Last_Name CHAR(25),
-            Score INT
-        ); """
- 
-cursor_obj.execute(table)
- 
-print("Table is Ready")
